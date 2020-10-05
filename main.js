dcbebc98ed8fe02ab3e66f19c07056064a7038ed
Gregory.Johnson <3531op@gmail.com>
2020-10-05 14:36:58 -0400

    Back to you Pat

--- main.js
+++ main.js
@@ -5,4 +5,29 @@ function logLetters(name) {
     i++
   }
 }
-logLetters("Greg")
+// logLetters("Greg")
+
+function everyThird(str) {
+  let i = 2;
+  while( i < str.length ) {
+    console.log( str[i] );
+    i += 3;
+  }
+}
+// everyThird("I am the alfalfa and the omelette.")
+
+
+function logLettersBackwards( str ) {
+  let i = str.length - 1
+  while (i >= 0) {
+    console.log(str [i]);
+    i--
+  }
+}
+// logLettersBackwards('hello')
+// logLettersBackwards('oh hi')
+
+
+
+
+
